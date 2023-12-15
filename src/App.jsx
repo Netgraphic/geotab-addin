ddf9765f835722934d8558909068625e6f7f1bdb
Diego Moris <diego.mt91@gmail.com>
2023-12-15 09:08:39 +0100

    Routes deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,77 @@
-import { Route, Routes } from "react-router-dom";
-import Setup from "./routes/Setup";
+import { useForm, FormProvider, useFormState } from "react-hook-form";
+import { faGears } from "@fortawesome/free-solid-svg-icons";
+
+import ConfigStatus from "./components/setup/ConfigStatus";
+import Tabs from "./components/setup/Tabs";
+import WelcomeConfig from "./components/setup/WelcomeConfig";
+import Title from "./components/Title";
+
+const CheckForm = ({ control }) => {
+  const { isSubmitted, isSubmitSuccessful } = useFormState({
+    control,
+  });
+
+  return (
+    isSubmitted &&
+    !isSubmitSuccessful && (
+      <p className="errorSubmission">Hay errores en la configuración</p>
+    )
+  );
+};
 
 const App = () => {
+  const methods = useForm({
+    defaultValues: {
+      orderSurfsight: "si",
+      whatsappNotifications: "no",
+      emailNotifications: "no",
+      identifySoundsQuantity: 0,
+      identifySoundsDuration: 0.5,
+      nonIdentifySoundsQuantity: 0,
+      nonIdentifySoundsDuration: 0.5,
+      driverSoundsQuantity: 0,
+      driverSoundsDuration: 0.5,
+      autoLogout: "no",
+      logoutTime: "",
+    },
+  });
+
+  //Don't submit when Enter is pressed on
+  const checkKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <main>
-      <Routes>
-        <Route path="/" element={<Setup />} />
-      </Routes>
-    </main>
+    <>
+      <WelcomeConfig />
+      <ConfigStatus />
+
+      <FormProvider {...methods}>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          onKeyDown={(e) => checkKeyDown(e)}
+          noValidate
+          style={{ overflow: "hidden" }}
+        >
+          <div className="container-header">
+            <button type="submit" className="btn-alt">
+              Guardar
+            </button>
+            <CheckForm control={methods.control} />
+            <div className="clear"></div>
+          </div>
+
+          <Title text="Configuración" icon={faGears} />
+          <Tabs />
+        </form>
+      </FormProvider>
+    </>
   );
 };
 
